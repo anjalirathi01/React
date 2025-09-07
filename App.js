767583009dc7934bbd216ef72be35cf39a4cce8c
anjalirathi01 <anjalir1101@gmail.com>
2025-09-07 19:06:54 +0530

    Day 4 restrauant app

---
+++ App.js
@@ -0,0 +1,597 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const styleCard = {
+       backgroundColor: "#f0f0f0"
+}
+
+const resObj = [
+    {
+"info": {
+"id": "688748",
+"name": "McDonald's",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/5/22/549cbc31-66cf-4535-b37a-59f937f3705d_688748.JPG",
+"locality": "Shivaji Nagar",
+"areaName": "Parijat Nagar",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Burgers",
+"Beverages",
+"Cafe",
+"Desserts"
+],
+"avgRating": 4.6,
+"veg": true,
+"parentId": "630",
+"avgRatingString": "4.6",
+"totalRatingsString": "1.8K+",
+"sla": {
+"deliveryTime": 18,
+"lastMileTravel": 0.7,
+"serviceability": "SERVICEABLE",
+"slaString": "15-20 mins",
+"lastMileTravelString": "0.7 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-09-07 23:45:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "Rxawards/_CATEGORY-Burger.png",
+"description": "Delivery!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Rxawards/_CATEGORY-Burger.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹119"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "4.3",
+"ratingCount": "4.2K+"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-b4360a38-4457-4a91-92fc-033feb8e0888"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nashik/mcdonalds-shivaji-nagar-parijat-nagar-rest688748",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "790313",
+"name": "KFC",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/6876e7bf-bc38-4484-a3b0-176e16fe20b4_790313.JPG",
+"locality": "Lawate Nagar",
+"areaName": "Center Mall",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Burgers",
+"Fast Food",
+"Rolls & Wraps"
+],
+"avgRating": 4.4,
+"parentId": "547",
+"avgRatingString": "4.4",
+"totalRatingsString": "1.5K+",
+"sla": {
+"deliveryTime": 22,
+"lastMileTravel": 1,
+"serviceability": "SERVICEABLE",
+"slaString": "20-25 mins",
+"lastMileTravelString": "1.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-09-07 23:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "Rxawards/_CATEGORY-Burger.png",
+"description": "Delivery!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Rxawards/_CATEGORY-Burger.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "50% OFF",
+"subHeader": "UPTO ₹80"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-b4360a38-4457-4a91-92fc-033feb8e0888"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nashik/kfc-lawate-nagar-center-mall-rest790313",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "806680",
+"name": "Burger King",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/90b5a960-04a3-45f7-ade9-679fac64936d_806680.jpg",
+"locality": "3rd floor",
+"areaName": "Parijat Nagar",
+"costForTwo": "₹350 for two",
+"cuisines": [
+"Burgers",
+"American"
+],
+"avgRating": 4.4,
+"parentId": "166",
+"avgRatingString": "4.4",
+"totalRatingsString": "4.7K+",
+"sla": {
+"deliveryTime": 25,
+"lastMileTravel": 1,
+"serviceability": "SERVICEABLE",
+"slaString": "25-30 mins",
+"lastMileTravelString": "1.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-09-08 01:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "Rxawards/_CATEGORY-Burger.png",
+"description": "Delivery!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Rxawards/_CATEGORY-Burger.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹59"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-b4360a38-4457-4a91-92fc-033feb8e0888"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nashik/burger-king-3rd-floor-parijat-nagar-rest806680",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "697245",
+"name": "Subway",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/12/931de12e-3687-46c6-8de0-a05c93398fcd_697245.jpg",
+"locality": "Lawate nagar",
+"areaName": "City Centre",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"sandwich",
+"Salads",
+"wrap",
+"Healthy Food"
+],
+"avgRating": 4.3,
+"parentId": "2",
+"avgRatingString": "4.3",
+"totalRatingsString": "1.6K+",
+"sla": {
+"deliveryTime": 27,
+"lastMileTravel": 1,
+"serviceability": "SERVICEABLE",
+"slaString": "25-30 mins",
+"lastMileTravelString": "1.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-09-07 23:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹69"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-b4360a38-4457-4a91-92fc-033feb8e0888"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nashik/subway-lawate-nagar-city-centre-rest697245",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "278214",
+"name": "Theobroma",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/8/14/bcbe0152-9ef8-43ea-b422-0fd0802a0288_278214.JPG",
+"locality": "Parijat Nagar",
+"areaName": "Parijat Nagar",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Bakery",
+"Desserts"
+],
+"avgRating": 4.6,
+"parentId": "1040",
+"avgRatingString": "4.6",
+"totalRatingsString": "2.0K+",
+"sla": {
+"deliveryTime": 17,
+"lastMileTravel": 0.7,
+"serviceability": "SERVICEABLE",
+"slaString": "15-20 mins",
+"lastMileTravelString": "0.7 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-09-07 23:59:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "Rxawards/_CATEGORY-Desserts.png",
+"description": "Delivery!"
+},
+{
+"imageId": "newg.png",
+"description": "Gourmet"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Rxawards/_CATEGORY-Desserts.png"
+}
+},
+{
+"attributes": {
+"description": "Gourmet",
+"imageId": "newg.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹99"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "3.9",
+"ratingCount": "133"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-b4360a38-4457-4a91-92fc-033feb8e0888"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nashik/theobroma-parijat-nagar-rest278214",
+"type": "WEBLINK"
+}
+},
+{
+"info": {
+"id": "217029",
+"name": "Domino's Pizza",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/8/8/4ec0e996-8314-4ff8-abb0-917808add08c_217029.JPG",
+"locality": "City Center Mall",
+"areaName": "Parijat Nagar",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Pizzas",
+"Italian",
+"Pastas",
+"Desserts"
+],
+"avgRating": 4.1,
+"parentId": "2456",
+"avgRatingString": "4.1",
+"totalRatingsString": "1.9K+",
+"sla": {
+"deliveryTime": 25,
+"lastMileTravel": 1,
+"serviceability": "SERVICEABLE",
+"slaString": "20-25 mins",
+"lastMileTravelString": "1.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-09-07 22:55:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "android/static-assets/icons/big_rx.png",
+"description": "bolt!"
+},
+{
+"imageId": "Rxawards/_CATEGORY-Pizza.png",
+"description": "Delivery!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "android/static-assets/icons/big_rx.png"
+}
+},
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Rxawards/_CATEGORY-Pizza.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹69"
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-b4360a38-4457-4a91-92fc-033feb8e0888"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/nashik/dominos-pizza-city-center-mall-parijat-nagar-rest217029",
+"type": "WEBLINK"
+}
+},
+]
+
+const Header = () => {
+       return(
+              <div className="header">
+                     <div className="logo-container">
+                            <img className="logo" src="https://t3.ftcdn.net/jpg/08/29/90/88/360_F_829908823_kYsRKdQcIaYEAhHRAZTIXuSKvuVPif8w.jpg"></img>
+                     </div>
+                     <div className="nav-items">
+                            <ul>
+                                   <li>Home</li>
+                                   <li>About Us</li>
+                                   <li>Contact Us</li>
+                                   <li>Cart</li>
+
+                            </ul>
+                     </div>
+              </div>
+       )
+};
+
+const RestaurantCard = (props) => {
+       const { resData} = props;
+       const {name, cuisines, avgRating, deliveryTime, costForTwo} = resData.info;
+       console.log(props);
+       return (
+              <div className="res-card" style={styleCard}>
+                     <img className="res-logo" alt="res-logo" 
+                     src = {"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resData.info.cloudinaryImageId}></img>
+                     <h3>{name}</h3>
+                     <h4>{cuisines.join(",")}</h4>
+                     <h4>{avgRating} stars</h4>
+                     <h4>{deliveryTime} minutes</h4>
+                     <h4>{costForTwo}</h4>
+              </div>
+       )
+}
+
+const Body = () => {
+       return (
+              <div className="body">
+                     <div className="search">Search</div>
+                     <div className="res-container">
+                            {resObj.map((restaurant) => (
+                              <RestaurantCard resData= {restaurant}/>
+                            ))}
+                            {/* <RestaurantCard resData={resObj[0]}/>
+                            <RestaurantCard resData={resObj[1]}/>
+                            <RestaurantCard resData={resObj[2]}/>
+                            <RestaurantCard resData={resObj[3]}/>
+                            <RestaurantCard resData={resObj[4]}/>
+                            <RestaurantCard resData={resObj[5]}/> */}
+                            {/* <RestaurantCard resName="XYZ" cusine="xyz"/> */}
+                     </div>
+              </div>
+       )
+}
+const AppLayout = () => {
+       return (
+              <div className="app">
+               <Header/>
+               <Body/>
+              </div>
+       )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
+
+
+
